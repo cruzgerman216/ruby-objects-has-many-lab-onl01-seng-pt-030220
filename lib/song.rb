b2a9effa95163165351e6bdfca0418d7eed55747
German Cruz <cruzgerman216@yahoo.com>
2020-03-16 01:34:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,11 @@
 class Song
   attr_accessor :artist, :title
-    @@all = []
   def initialize(title)
     @title = title
+
   end
 
-  def save
-  @@all << self
-  end
 
-  def self.all
-    @@all
-  end
+
+
 end
