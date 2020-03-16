2d09b3c0bb9b43c17a4d890bde2a9cec21894455
German Cruz <cruzgerman216@yahoo.com>
2020-03-16 01:33:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,10 +3,12 @@ class Artist
 
   def initialize(name)
      @name = name
-
+     @songs = []
    end
    def add_song(song)
      @songs << song
    end
-
+   def songs
+       @songs
+     end
 end
